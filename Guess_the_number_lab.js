65454e1756fc196c83beeaa95c7b455af0b6b576
anami823 <anami823@gmail.com>
2019-07-24 16:25:51 -0700

    Add prevGueses property

--- Guess_the_number_lab.js
+++ Guess_the_number_lab.js
@@ -21,6 +21,7 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    prevGuesses: [],
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
