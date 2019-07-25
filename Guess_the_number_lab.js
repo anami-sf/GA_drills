bc2ba52eed96bc0a8bb4c52e354feea45e6d59ca
anami823 <anami823@gmail.com>
2019-07-24 19:43:31 -0700

    Add a  method to  that  will call after a guess has been made that alerts player if number is high, low or correct

--- Guess_the_number_lab.js
+++ Guess_the_number_lab.js
@@ -22,39 +22,52 @@ const game = {
     smallestNum: 1,
     secretNum: null,
     prevGuesses: [],
-    play: function() {
-        this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-
-        this.prevGuesses.push( this.getGuess() )
-    },
     getGuess: function(){
         const guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}: `)
         return guess
+    },
+    play: function() {
+        this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+        const guess = this.getGuess()
+        game.prevGuesses.push(guess)
+        game.render(guess)
+    },
+    render: function(input) {
+        if (input > game.secretNum) {
+            alert(`Your guess is too high\n` + 'Previous guesses: ' + game.prevGuesses)
+        } else if (input < game.secretNum) {
+            alert(`Your guess is too low\n` + 'Previous guesses: ' + game.prevGuesses)
+        } else if (input == game.secretNum) {
+            alert(`Congratulations! You won in ${game.prevGuesses.length} guesses.`)
+        }
     }
+  
   };
 
 game.play()
-console.log(game.prevGuesses)
-//console.log(game.getGuess())
+//game.render(9)
+console.log(game.secretNum)
+//console.log('prev guesses: '  + game.prevGuesses)
+//console.log('guess: ' + game.getGuess())
 
-var input = ''
+var guess = ''
 var guesses = []
 var count = 0
 
-/* while (input !== 'q') {
+/* while (guess !== 'q') {
     console.log(game.secretNum)
-    input = prompt('Enter a number or "Q": ')
-    guesses.push(input)
+    guess = prompt('Enter a number or "Q": ')
+    guesses.push(guess)
     console.log(guesses)
 
-    if (input > game.secretNum) {
+    if (guess > game.secretNum) {
         count += 1
-        alert(`Too high\n` + 'Guesses: ' + guesses)
-    } else if (input < game.secretNum) {
+        alert(`Too high\n` + 'Previous guesses: ' + guesses)
+    } else if (guess < game.secretNum) {
         count += 1
-        alert(`Too low\n` + 'Guesses: ' + guesses)
-    } else if (input == game.secretNum) {
+        alert(`Too low\n` + 'Previous guesses: ' + guesses)
+    } else if (guess == game.secretNum) {
         alert(`Congratulations! You won in ${count} guesses.`)
         break
     }
